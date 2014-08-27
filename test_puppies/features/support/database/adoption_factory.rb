ad696183e1cd9cec55eca380fe423de98f1bf476
Jimi Odeyemi <jimode68@gmail.com>
2014-08-27 22:44:59 +0100

    Add database cleaner

--- test_puppies/features/support/database/adoption_factory.rb
+++ test_puppies/features/support/database/adoption_factory.rb
@@ -1,15 +1,17 @@
-FactoryGirl.define do
-	factory :order do
-		name 'Cheezy'
-		address '123 Main'
-		email 'cheezy@me.com'
-		pay_type 'Check'
-	end
+require_relative 'puppy'
 
-	factory :adoption do
-		association :order
-		puppy Puppy.find_by_name('Hanna')
-	end
+FactoryGirl.define do
+  factory :order do
+    name 'Cheezy'
+	address '123 Main'
+	email 'cheezy@me.com'
+	pay_type 'Check'
+  end
+
+  factory :adoption do
+    association :order
+	puppy Puppy.find_by_name('Hanna')
+  end
 end
 
 World(FactoryGirl::Syntax::Methods)
